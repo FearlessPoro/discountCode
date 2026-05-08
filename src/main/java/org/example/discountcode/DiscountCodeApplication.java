1589d020beaa3e287e53a58260abc48dddd628bb
FearlessPoro <Csiticomsea1>
2026-05-08 21:55:05 +0200

    Intial commit

---
+++ src/main/java/org/example/discountcode/DiscountCodeApplication.java
@@ -0,0 +1,13 @@
+package org.example.discountcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscountCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscountCodeApplication.class, args);
+    }
+
+}
